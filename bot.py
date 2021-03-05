1362ba0d13a4f33f55aa2d6d95fd332a5fcc81b4
CyberRex <hspwinx86@gmail.com>
2021-03-05 21:29:25 +0900

    ThreadPoolExecutorでフリーズ回避

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
 import config
 import discord
 import time
+import gc
+import concurrent.futures
 from discord.ext import commands, tasks
 
 import mcipc.query
 
+startedFlag = False
+
 bot = commands.Bot(command_prefix=config.prefix, intents=discord.Intents.default())
 
-@tasks.loop(seconds=30)
-async def server_status_updater():
-  
+def ssu_coproc():
   status = None
   
   try:
@@ -17,17 +19,31 @@ async def server_status_updater():
     with mcipc.query.Client(config.host, config.port) as mcbe:
       status = mcbe.stats(full=True)
       ping = (time.time()-pt)*1000
+    del mcbe
+    gc.collect()
       
   except:
-    await bot.change_presence(status=discord.Status.dnd, activity=discord.Game('サーバーがオフラインです'))
+    asyncio.create_task(bot.change_presence(status=discord.Status.dnd, activity=discord.Game('サーバーがオフラインです')))
     return
   
   st = f'{status.num_players} / {status.max_players} | ping: {ping:.1f}ms'
-  await bot.change_presence(status=discord.Status.online, activity=discord.Game(st))
+  asyncio.create_task(bot.change_presence(status=discord.Status.online, activity=discord.Game(st)))
+
+@tasks.loop(seconds=30)
+async def server_status_updater():
+  
+  with concurrent.futures.ProcessPoolExecutor() as pool:
+    bot.loop.run_until_complete(loop.run_in_executor(pool, ssu_coproc))
 
 @bot.event
 async def on_ready():
+  global startedFlag
   
+  if startedFlag:
+   return
+
+  startedFlag = True
+
   server_status_updater.start()
   
   print('準備ができました')
